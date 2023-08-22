9f754585f440165760fb2c887eb29ceb9c591cf1
Breno Miranda <brenoafmiranda@gmail.com>
2023-08-22 20:39:42 -0300

    Added method addition3

--- calculator.py
+++ calculator.py
@@ -2,6 +2,9 @@ class Calculator():
 
     def addition(self, a, b):
         return a + b
+
+    def addition3(self, a, b, c):
+        return a + b + c
     
     def subtraction(self, a, b):
         return a - b
